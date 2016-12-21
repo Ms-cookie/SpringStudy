bf551cb0d4ccb83deccc2c2fb1f2b9d46c8261fe
KooMinJung <koolove_it@naver.com>
2016-12-21 10:48:50 +0900

    1. 초난감 DAO

--- src/main/java/springbook/user/domain/User.java
+++ src/main/java/springbook/user/domain/User.java
@@ -1,8 +1,7 @@
 package springbook.user.domain;
 
 public class User {
-
-    // 사용자 정보를 저장할 때는 자바빈 규약을 따르는 Object를 이용하면 편리하다
+    /** 사용자 정보를 저장할 때는 자바빈 규약을 따르는 Object를 이용하면 편리하다*/
     private String id;
     private String name;
     private String passwd;
